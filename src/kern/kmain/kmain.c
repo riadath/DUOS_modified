6cf5d8b9735986f36f19b4239a633f5055ddf27b
riadath <akib152012@gmail.com>
2023-07-22 02:22:47 +0600

    gdb automation

--- src/kern/kmain/kmain.c
+++ src/kern/kmain/kmain.c
@@ -39,26 +39,10 @@
 
 void kmain(void)
 {
-__sys_init();
+	__sys_init();
+	kprintf("____DRIVER CODE START____\n");
 
-//uint32_t b=0;
-float x=50.59;
-uint8_t y=23,f=56;
-x++;
-kprintf("%d %d %f\n",y,f,x);
-kprintf("After Input\n");
-//uint8_t p[8]="1234.34\0";
-//x=str2float(p);
-kprintf("After Input\n");
-kprintf("Time Elapse %d ms\n",__getTime());
-while(1){
-//	kprintf((uint8_t*)"%d",(uint8_t*)a);
-//	kscanf((uint8_t*)"%d",(uint8_t*)b);
-//	kprintf((uint8_t*)"%d",(uint8_t*)b);
-//	a++;
-//	b++;
-	//you can change the following line by replacing a delay function
-	//for(uint32_t i=0;i<100000000;i++){kprintf("Time Elapse %d ms\n",__getTime());}	
-}
+	kprintf("____DRIVER CODE END____\n");
+
 }
 
