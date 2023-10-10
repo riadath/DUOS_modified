bde5e40497d8f5294dbeaa614c83ab9f0240701e
riadath <akib152012@gmail.com>
2023-10-11 02:28:43 +0600

    delete

--- duos/src/userland/include/ustdio.h
+++
@@ -1,7 +0,0 @@
-#ifndef __USTDIO_H
-#define __USTDIO_H
-
-void uprintf(char *format,...);
-void uscanf(char *format, ...);
-void firstTest(char* args);
-#endif
--- duos/src/userland/include/ustring.h
+++
@@ -1,8 +0,0 @@
-#ifndef __USTRING_H
-#define __USTRING_H
-
-#include <stdarg.h>
-
-
-
-#endif
